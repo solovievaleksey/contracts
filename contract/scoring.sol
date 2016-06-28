bc5007c70ad115f6cc70a481b2bdf858c0921037
solovievaleksey <solovievaleksey@gmail.com>
2016-06-28 17:21:31 +0300

    add constant. update removeBank()

--- contract/scoring.sol
+++ contract/scoring.sol
@@ -22,7 +22,7 @@ contract CScoring {
 		suicide(m_owner);
 	}
 	
-	function bankByAddr(address addr) internal returns (uint index) {
+	function bankByAddr(address addr) internal constant returns (uint index) {
 		for (uint i=0;i<m_banks.length;i++) {
 			if (m_banks[i].m_addr == addr) {
 				return i;
@@ -41,23 +41,26 @@ contract CScoring {
 		
 	function removeBank(address addr) onlyOwner public {
 		if (addr == 0x0) throw;
-		uint index = bankByAddr(addr);
-		delete m_banks[index];
+		for(uint index = bankByAddr(addr);index < m_banks.length-1;++index) {
+			m_banks[index] = m_banks[index+1];
+		}
+		m_banks.length-=1;
 	}
 	
 	function setScoring(bytes32 hash, uint value) public {
-		if(hash == 0x0) throw;
+		if(hash == 0x0 || value == 0) throw;
 		uint index = bankByAddr(msg.sender);
 		m_banks[index].m_scoring[hash] = value;
 	}
 	
-	function scoring(bytes32 hash) public returns (uint value) {
+	function scoring(bytes32 hash) public constant returns (uint value) {
 		if(hash == 0x0) throw;
 		uint index = bankByAddr(msg.sender);
 		return m_banks[index].m_scoring[hash];
 	}
 	
-	function scoringAll(bytes32 hash) public returns(uint[] value) {
+	function scoringAll(bytes32 hash) public constant returns(uint[] value) {
+		if(hash == 0x0) throw;
 		uint[] memory values = new uint[](m_banks.length);
 		for (uint i=0;i<m_banks.length;i++) {
 			values[i] = m_banks[i].m_scoring[hash];
