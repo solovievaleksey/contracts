bc822f1dbd6b9ce6796f7600f1d595b75c243dcc
solovievaleksey <solovievaleksey@gmail.com>
2016-07-06 19:10:15 +0300

    add CProxyContract

---
+++ contract/proxycontract.sol
@@ -0,0 +1,38 @@
+//Sample proxy contract
+
+contract CProxyInterface {
+	function set(uint value);
+	function get() constant returns(uint value);
+}
+
+contract CProxyImplementation is CProxyInterface {
+	uint private m_value;
+	
+	function set(uint value) {
+		m_value = value;
+	}
+	
+	function get() constant returns(uint value){
+		return m_value;
+	}	
+}
+
+contract CProxyContract {
+	address private m_proxy;
+	
+	function CProxyContract() { }
+	
+	function setProxy(address addr) {
+		m_proxy = addr;
+	}
+	
+	function set(uint value) {
+		if (m_proxy == 0) throw;
+		CProxyInterface(m_proxy).set(value);		
+	}
+	
+	function get() constant returns(uint value) {
+		if (m_proxy == 0) throw;
+		return CProxyInterface(m_proxy).get();
+	}
+}
